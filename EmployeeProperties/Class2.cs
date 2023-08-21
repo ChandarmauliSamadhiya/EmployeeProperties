58c5804d9240faba8b6b1eba8b64382fd861f7aa
acer <acer@ASUR>
2023-08-21 09:28:32 +0530

    Add project files.

---
+++ EmployeeProperties/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public partial class Employee
+{
+    public double CalculateSalary()
+    {
+       
+        double totalSalary = BaseSalary + CalculateBonus() - CalculateDeductions();
+        return totalSalary;
+    }
+
+    private double CalculateBonus()
+    {
+
+        return BaseSalary * 0.1;
+    }
+
+    private double CalculateDeductions()
+    {
+        
+        return 200;
+    }
+}
---
+++ EmployeeProperties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+        employee.FirstName = "John";
+        employee.LastName = "Doe";
+        employee.Age = 30;
+        employee.BaseSalary = 50000;
+
+        double salary = employee.CalculateSalary();
+        Console.WriteLine($"Employee {employee.FirstName} {employee.LastName} earns a salary of ${salary}");
+        Console.ReadLine();
+    }
+}
